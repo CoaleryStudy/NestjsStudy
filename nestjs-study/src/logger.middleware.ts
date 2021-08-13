0dbabff7ebe680c61e8b30f9a8b0cbcf32f9e2f7
러리 <doralife12@naver.com>
2021-08-13 22:55:33 +0900

    Feat: middleware

---
+++ nestjs-study/src/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleWare implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction): void {
+    console.log('Request...');
+    next();
+  }
+}
