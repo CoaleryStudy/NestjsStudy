cb0b6d237b985059489c76f7c1f8aa32c88ad4e6
러리 <doralife12@naver.com>
2021-08-16 01:33:43 +0900

    Feat: Exception filter

--- nestjs-study/src/app.module.ts
+++ nestjs-study/src/app.module.ts
@@ -4,11 +4,19 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { CatsModule } from './cats/cats.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 import { LoggerMiddleWare } from './logger.middleware';
 
 @Module({
   imports: [CatsModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- nestjs-study/src/cats/cats.controller.ts
+++ nestjs-study/src/cats/cats.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat-dto';
+import { ForbiddenException } from './forbidden.exception';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
+@UseFilters(HttpExceptionFilter)
 @Controller({ path: 'cats' })
 export class CatsController {
   constructor(private catsService: CatsService) {}
@@ -15,5 +26,23 @@ export class CatsController {
   @Get()
   async findAll(): Promise<Array<Cat>> {
     return this.catsService.findAll();
+  }
+
+  @Get('exception')
+  throwSomeException(): never {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+  }
+
+  @Get('customEx')
+  throwSomeCustomException(): never {
+    throw new HttpException(
+      { status: HttpStatus.FORBIDDEN, error: 'This is a custom message' },
+      HttpStatus.FORBIDDEN,
+    );
+  }
+
+  @Get('customExClass')
+  throwSomeCustomExceptionCalss(): never {
+    throw new ForbiddenException();
   }
 }
