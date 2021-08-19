20142f2cf6c4d790c5621c1d4a2c60f2cf86effe
러리 <doralife12@naver.com>
2021-08-20 01:54:27 +0900

    Feat: Pipes

--- nestjs-study/src/cats/cats.controller.ts
+++ nestjs-study/src/cats/cats.controller.ts
@@ -1,17 +1,22 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   HttpException,
   HttpStatus,
+  Param,
+  Query,
+  ParseIntPipe,
   Post,
   UseFilters,
 } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
 import { CatsService } from './cats.service';
-import { CreateCatDto } from './dto/create-cat-dto';
+import { CreateCatDto } from './dto/create-cat.dto';
 import { ForbiddenException } from './forbidden.exception';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { ValidationPipe } from 'src/validation.pipe';
 
 @UseFilters(HttpExceptionFilter)
 @Controller({ path: 'cats' })
@@ -19,13 +24,23 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
 
   @Get()
   async findAll(): Promise<Array<Cat>> {
     return this.catsService.findAll();
+  }
+
+  @Get('hello')
+  test(@Query('id', new DefaultValuePipe(0), new ParseIntPipe()) id: number) {
+    return { hi: 'hello', id };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseIntPipe()) id: number): Promise<Cat> {
+    return this.catsService.findOne(id);
   }
 
   @Get('exception')
